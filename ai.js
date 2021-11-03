f9a6832da0936d61411d7b8309d2b2b1d67aabaf
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2021-11-03 14:27:49 -0500

    ai.js folder

--- ai.js
+++ ai.js
@@ -1,5 +1,20 @@
+"use strict"
+
+const prompt = require ("prompt-sync")();
+const {Player}= require ("./Player");
 class Ai extends Player {
-    constructor(name){
-        super(name);
+    constructor() {
+        console.log("Prepare to face your Destroyer");
+        let aiName = prompt("Terminal Illness");
+        this.aiRandomSelect = Math.floor(Math.random()  * 5); 
+        super(aiName);
     }
+
+    /*aiRandomSelect(aiRandomGestures){
+        
+    }    
+    */
+}
+module.exports = {
+    aiName: aiName
 }
