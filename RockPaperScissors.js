a0bdada890550eb9b754b1bb3f68536683edd644
Stanley Pethel <stanleypethel@yahoo.com>
2021-11-04 12:03:40 -0400

    updated

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -7,25 +7,20 @@ class rockPaperScissors{
     constructor (){
         this.Player1;
         this.Player2;
-        this.AI;
 
     }
     
     displayWelcome(){
         console.log("Let's Play A Game of Rock, Paper, Scissors!");
-        console.log(`Which player do you choose to play? \n 1 - ${this.player1.name} vs ${this.player2.name} \n 2 - ${this.player1.name} vs ${this.ai.name}`);
+        console.log(`Which player do you choose to play? \n 1 - ${this.player1.name} vs ${this.player2.name}`);
         this.player1 = new Human1();
         this.player2 = new Human1();
-        this.AI = new Ai();
     }
     runGamePlay(){
-        console.log(`Let the game between ${this.player1} and ${this.player2} or ${this.Ai} Begin!!`);
-        let userChoice = 
-        let userChoice = prompt(this.gestures);
-        if(userChoice == []){
-            this.player1.shoot(this.player2 || this.Ai);
-        }
-        else if{}
+        console.log(`Let the game between ${this.player1} and ${this.player2} Begin!!`);
+        
+        
+      
     }
 }
 
--- human1.js
+++ human1.js
@@ -10,7 +10,12 @@ constructor (){
     let playerName2 = prompt();
     super(playerName1, playerName2);
 
-    }      
+    }    
+    chooseGesture(){
+        console.log(this.gestures);
+        let userInput = prompt("Choose which gesture you want \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n 4 - Lizard \n 5 - Spock" );
+
+        }  
 }
 module.exports = {
     Human1: Human1
