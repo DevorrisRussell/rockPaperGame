9e18da8e4cf7da0c6e257c34bda94db6f6a35bb9
Stanley Pethel <stanleypethel@yahoo.com>
2021-11-04 11:20:12 -0400

    updated

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -7,6 +7,7 @@ class rockPaperScissors{
     constructor (){
         this.Player1;
         this.Player2;
+        this.AI;
 
     }
     
@@ -14,7 +15,17 @@ class rockPaperScissors{
         console.log("Let's Play A Game of Rock, Paper, Scissors!");
         console.log(`Which player do you choose to play? \n 1 - ${this.player1.name} vs ${this.player2.name} \n 2 - ${this.player1.name} vs ${this.ai.name}`);
         this.player1 = new Human1();
-        this.player2 = new Ai();
+        this.player2 = new Human1();
+        this.AI = new Ai();
+    }
+    runGamePlay(){
+        console.log(`Let the game between ${this.player1} and ${this.player2} or ${this.Ai} Begin!!`);
+        let userChoice = 
+        let userChoice = prompt(this.gestures);
+        if(userChoice == []){
+            this.player1.shoot(this.player2 || this.Ai);
+        }
+        else if{}
     }
 }
 
--- player.js
+++ player.js
@@ -2,10 +2,16 @@
 class Player {
     constructor(name){
     this.name = name;
-        
-    } 
-
+    this.score = 0;
+    this.selectedGesture = "";
+    this.gestures = ["Rock", "Paper", "Scissors", "Lizzard", "Spock"];
+        }
+        chooseGesture(){
+            console.log(this.gestures);
 }
+}
+
+
 
 
 module.exports = {
