8503ebf2f544e883f220e7662a6c3b025b83a604
Devorris Russell <devorris_r@yahoo.com>
2021-11-04 22:44:49 -0400

    added if statement

--- ai.js
+++ ai.js
@@ -6,26 +6,16 @@ const {Player}= require ("./Player");
 class Ai extends Player {
 
     constructor() {
-        console.log("Prepare to face your Destroyer");
-<<<<<<< HEAD
-        
-        
-        super(aiName);
+        console.log("Prepare to face your Destroyer")
+        let computer = "Terminal Illness";
+        let selectedGesture = Math.floor(Math.random() * 5);
+        super(computer, selectedGesture);
     }
 
-    aiRandomSelect(aiRandomGestures){
-     this.aiRandomSelect = Math.floor(Math.random()  * 5); 
-      console.log(this.gestures);
         
     }    
     
-=======
-        let computer = "Terminal Illness";
-        let selectedGesture = Math.floor(Math.random()  * 5); 
-        super(computer, selectedGesture);
-    }
->>>>>>> 480cf4e3ac53e5011dd98fa712c5058ea0e1eb36
-}
+
 
 module.exports = {
     Ai: Ai
